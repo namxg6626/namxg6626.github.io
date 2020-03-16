d8405d25de8e1ab89d991e391cf032498e1cb224
Lê Văn Nam <namxg6626@gmail.com>
2020-03-16 23:12:02 +0700

    create slide dynamically

--- hit_js/buoi1/advance.js
+++ hit_js/buoi1/advance.js
@@ -21,20 +21,26 @@ const slideStyle = [
   }
 ];
 
-const slides = document.querySelectorAll(".slide").forEach((slide, id) => {
-  slide.style.backgroundColor = slideStyle[id].bgColor;
-  slide.textContent = slideStyle[id].text;
-  console.log(slide);
-});
-
+const slideContainer = document.getElementsByClassName("slide-container")[0];
+init(slideContainer);
 let autoNextSlide = setInterval(nextSlide, 1000);
+
+function init(slideContainer) {
+  for (let ele of slideStyle) {
+    let slide = document.createElement("div");
+    slide.classList.add("slide");
+    slide.style.setProperty("background", ele.bgColor);
+    slide.textContent = ele.text;
+    slideContainer.appendChild(slide);
+  }
+}
 
 function nextSlide() {
   clearInterval(autoNextSlide);
   const slideContainer = document.querySelector(".slide-container");
   const currentPosition = parseInt(slideContainer.style.left);
 
-  if (currentPosition === -1200) slideContainer.style.left = 0;
+  if (currentPosition === -((slideStyle.length - 1) * 300)) slideContainer.style.left = 0;
   else slideContainer.style.left = currentPosition - 300 + "px";
   autoNextSlide = setInterval(nextSlide, 1000);
 }
